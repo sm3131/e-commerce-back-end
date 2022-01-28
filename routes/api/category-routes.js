2b816fa4e66c06160a63a2c7fae8e5f305026d21
sm3131 <sm2683@nau.edu>
2022-01-28 14:53:21 -0700

    created get routes for categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -11,22 +11,53 @@ router.get('/', (req, res) => {
       'id',
       'category_name'
     ],
-    include: [
-      {
-        model: Product,
-        attributes: ['product_name']
-      }
-    ]
+    include:
+    {
+      model: Product,
+      attributes: ['product_name']
+    }
+  })
+  .then(categoryData => res.json(categoryData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
   })
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: [
+      'id',
+      'category_name'
+    ],
+    include: {
+      model: Product,
+      attributes: ['product_name']
+    }
+  })
+  .then(categoryData => {
+    if (!categoryData) {
+        res.status(404).json({ message: 'No category found with this id' });
+        return;
+    }
+    res.json(categoryData);
+})
+.catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+});
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name
+  })
 });
 
 router.put('/:id', (req, res) => {
